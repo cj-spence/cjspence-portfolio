197d79c076ddd4dbde7d2e8f212fa5a89dd52f07
cj-spence <christopherspencejr@gmail.com>
2025-05-11 15:23:50 -0700

    project gh link, website link, name change

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import { RevealOnScroll } from "../components/RevealOnScroll";
+import { FaGithub } from "react-icons/fa";
 
 export const Projects = () => {
 
@@ -17,16 +18,20 @@ export const Projects = () => {
                     <div className="p-6 rounded-xl border border-[#F7EC59] hover:-translate-y-1 hover:shadow-[0_2px_8px_rgba(247,236,89,0.4)] transition-all">
 
                         <h3 className="text-xl font-bold text-[#F7EC59] text-center">
-                                Resistance Beyond the Court
+                                Our Ancestors Have Given Us An Assignment
                         </h3>
 
                         <p className="text-[#F8F7F9] mb-4 mt-4 text-center">
                                 An interactive webpage dedicated to support and document the oral histories of Herman Brown and Musheer Abdul-Jabbar, two African-American Saint Mary's College of California alumni from the 1960s who experienced heinous racism while playing for Saint Mary's D1 NCAA basketball team. 
                         </p>
 
-                        <p className="text-[#F8F7F9] text-sm mb-4 mt-4 text-center">
-                            Created in partnership with my ES-201 class, Herman, Musheer, and Professor Michael Viola.
-                        </p>
+                        <div className="flex flex-col text-center mt-4 mb-2">
+                            <a href="https://github.com/cj-spence/cs-capstone" target="_blank" className=""><FaGithub className="inline-block w-6 h-6 sm:w-8 sm:h-8 text-[#F8F7F9] hover:text-[#F7EC59] hover:scale-115 transition-transform"/></a>
+                            <a href="https://hermanandmusheer.com" target="_blank" className="text-center items-center justify-center text-[#F7EC59] underline decoration-dotted mb-4 mt-2 hover:scale-105 transition-transform">
+                                hermanandmusheer.com
+                            </a>
+                        </div>
+
 
                         <div className="flex flex-wrap gap-2 items-center justify-center">
                             {["React", "TailwindCSS", "JavaScript"].map((tech, key) => (
